32c99d6da4362f4dbec59886a49ae3cc52048225
Marcos Vinícius <marcosbailoni@hotmail.com>
2024-02-20 16:44:31 -0300

    Start test JUnit

--- test/campo_minado/modelo/CampoTeste.java
+++ test/campo_minado/modelo/CampoTeste.java
@@ -1,12 +1,44 @@
 package campo_minado.modelo;
 
-import campo_minado.modelo.Campo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 
 public class CampoTeste {
 	
-	private Campo campo = new Campo(3, 3);
+	private Campo campo;
 	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3, 3);
+	}
 	
+	@Test
+	void TesteVizinhoRealDistanciaEsquerda() {
+		Campo vizinho = new Campo(3,2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
 		
+	@Test
+	void testevizinhoRealDistancia1Direita() {
+		Campo vizinho = new Campo (3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
 	
+	@Test
+	void testevizinhoRealDistancia1Cima() {
+		Campo vizinho = new Campo (2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	@Test
+	void testevizinhoRealDistancia1Baixo() {
+		Campo vizinho = new Campo (4, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
 }
