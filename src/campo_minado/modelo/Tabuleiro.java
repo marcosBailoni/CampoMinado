c9a31356b8977759e9a7d1913b4715a867c75d38
Marcos Vinícius <marcosbailoni@hotmail.com>
2024-02-29 16:19:31 -0300

    Update Tabuleiro.java
    update in tabuleiro,
    add methods
    gerarCampos
    associarVizinhos
    sortearMinas
    Reiniciar

--- src/campo_minado/modelo/Tabuleiro.java
+++ src/campo_minado/modelo/Tabuleiro.java
@@ -2,6 +2,9 @@ package campo_minado.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+
+import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
 
 public class Tabuleiro {
 	
@@ -23,14 +26,62 @@ public class Tabuleiro {
 	
 	
 	private void gerarCampos() {
-		
+		for(int linha = 0; linha < linhas; linha++) {
+			for(int coluna = 0; coluna < colunas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}
+		}
 	}
 	
+//	metodo abaixo: 
+//	Para cada campo (chamado de c1) em campos (lista)
+//	Percorra cada campo (chamado de c2) em campos (lista)
+//	Adicione cada campo da segunda pesquisa como vizinho de cada campo da primeira pesquisa 
+//	Campo 0 adicionar vizinho, campo 0 // campo 0 adicionar vizinho campo1...... 
+//	campo 1 adicionar vizinho, campo 0 // campo 1 adicionar vizinho campo 1.......
+//	........
+//	em código: c2, adicionado como vizinho de c1
+//	
+//	O metodo abaixo em resumo é a mesma coisa que: 
+	
+//	private void associarOsVizinhos() {
+//	    for(int i = 0; i < campos.length; i++) {
+//	        Campo c1 = campos[i];
+//	        for(int j = 0; j < campos.length; j++) {
+//	            Campo c2 = campos[j];
+//	            c1.adicionarVizinho(c2);
+//	        }
+//	    }
+//	}
 	private void associarOsVizinhos() {
-		
+		for(Campo c1: campos) {
+			for(Campo c2: campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
 	}
 
 	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+		
+		do {
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+		} while(minasArmadas < minas);
+	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+	
+	public String toString() {
 		
 	}
 }
